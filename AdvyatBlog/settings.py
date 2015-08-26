d2d318fa571f865191e2eaf7cff38186d1578287
Abhinav Kumar <abhinav@cloudthing.com>
2015-08-26 15:27:10 +0530

    changed db

--- AdvyatBlog/settings.py
+++ AdvyatBlog/settings.py
@@ -14,9 +14,9 @@ BLOGDIR = os.path.dirname(os.path.normpath(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'Advyat',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'advyat_blog',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'RANDOM',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
